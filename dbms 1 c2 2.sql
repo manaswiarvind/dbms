aa25e3110d07b84e246477ccebfa0a0dfbe11450
manaswiarvind <manaswiarvind.cs24@bmsce.ac.in>
2025-10-16 09:56:32 +0530

    Add files via upload

--- dbms 1 c2 2.sql
+++ dbms 1 c2 2.sql
@@ -1,5 +1,5 @@
 use newdatabase;
-create table branch(branchname varchar(30),branchcity varchar(30) ,assessts real,primary key(branchname));
+create table branch(branchname varchar(30),branchcity varchar(30) ,assessts real,primary key(branchname) );
 desc branch;
 create table bankaccount(accno integer,branchname varchar(30),balance real,primary key(accno),foreign key(branchname) references branch(branchname));
 desc bankaccount;
@@ -10,9 +10,20 @@ desc depositer;
 create table loan(loannumber int,branchname varchar(30),amount real,primary key(loannumber),foreign key(branchname)references branch(branchname));
 desc loan;
 
+create table borrower(customername varchar(30),loannumber integer,foreign key(customername) references bankcustomer(customername),foreign key(loannumber) references loan(loannumber)) ;
+desc borrower;
+drop table borrower;
+insert into borrower values("Avinash",1);
+insert into borrower values("Dinesh",2);
+insert into borrower values("Mohan",3);
+insert into borrower values("Nikhil",4);
+insert into borrower values("ravi",5);
+select* from borrower;
 select*  from branch;
 select* from loan;
+select* from bankcustomer;
 select* from bankaccount;
+select* from depositer;
 insert into branch values('SBI_Chamrajpet','bangalore',50000);
 insert into branch values('SBI_ResidencyRoad','bangalore',10000);
 insert into branch values('SBI_ShivajiRoad','Bombay',20000);
@@ -36,10 +47,25 @@ insert into depositer values('Avinash',1);
 insert into depositer values('Dinesh',2);
 insert into depositer values('Nikhil',4);
 insert into depositer values('Ravi',5);
-insert into depositer values('Avinash',8);
+insert into depositer values('Avinash',3);
 
 insert into bankcustomer values('Avinash','Bull_Temple_Road','Bangalore');
 insert into bankcustomer values('Dinesh','Bannergatta_Road','Bangalore');
 insert into bankcustomer values('Mohan','National_College_Road','Bangalore');
 insert into bankcustomer values('Nikhil','Akbar_Road','Delhi');
 insert into bankcustomer values('Ravi','Prithviraj_Road','Delhi');
+
+select distinct S.customername from depositer as S where not exists((select branchname from branch where branchcity='Delhi') except (select R.branchname from depositer as T ,bankaccount as R where T.accno=R.accno and S.customername =T.customername));
+
+select  distinct customername from borrower where customername not in ( select customername from depositer);
+
+select branchname from branch where assests> all(select assests from branch where branchcity='bangalore');
+
+select distinct customername from borrower,loan where borrower.loannumber=loan.loannumber and branchcity='bangalore' and(branchname,customername )in (select branchname,customername from depositer,bankaccount where depositer.accno=bankaccount.accno);
+
+  delete from bankaccount where branchname in(select branchname from branch where branchcity="bombay");
+
+update bankaccount set balance=balance+balance*(0.05);
+
+(SELECT CUSTOMERNAME FROM DEPOSITER ) UNION (SELECT
+CUSTOMERNAME FROM BORROWER);
